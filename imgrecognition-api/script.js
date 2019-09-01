c8e73dee5755fbd5ad297d1b153a26d8051dac4e
skylord <atulmehlasmart@gmail.com>
2019-09-01 15:38:34 -0230

    updated back-end

--- imgrecognition-api/script.js
+++ imgrecognition-api/script.js
@@ -24,7 +24,7 @@ const database = {
 };
 
 app.get("/", (req, res) => {
-  res.send("this is working");
+  res.send(database.users);
 });
 app.post("/signin", (req, res) => {
   if (req.body.email === database.users[0].email) {
@@ -33,9 +33,46 @@ app.post("/signin", (req, res) => {
     res.status(400).json("error loggin in");
   }
 });
+app.post("/register", (req, res) => {
+  database.users.push({
+    id: 125,
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const  {id} =req.params;
+  let found=false
+  database.users.forEach(user=>{
+    if(user.id==id){
+      found=true
+      return res.json(user);
+    }
+  })
+  if(!found){
+    res.status(404).json("no such user")
+  }
+});
+app.get("/profile/:id", (req, res) => {
+  const  {id} =req.params;
+  let found=false
+  database.users.forEach(user=>{
+    if(user.id==id){
+      found=true
+      return res.json(user);
+    }
+  })
+  if(!found){
+    res.status(404).json("no such user")
+  }
+});
 
 app.listen(3001, () => {
-  console.log("app is running");
 });
 
 // root = this is working (homepage) so get request
--- imgrecognition/src/components/Logo/Logo.js
+++ imgrecognition/src/components/Logo/Logo.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Tilt from "react-tilt";
 import brain from "../../img/brain.png";
+import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0 pa0" >
+    <div className="ma4 mt0 pa0">
       <Tilt
-        className="Tilt br2 shadow-2 pa0"
+        className="Tilt br2 shadow-2 pa0 logo"
         options={{ max: 55 }}
-        style={{ height: 150, width: 150,position:"absolute"}}
+        style={{ height: 150, width: 150, position: "absolute" }}
       >
         <div className="Tilt-inner">
           <img
